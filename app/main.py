4cd1b37777679e35f77b4cd1e1bd02ad8252f72d
Sondre Espe <Sondreespe02@gmail.com>
2025-12-11 14:26:50 +0100

    CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Forecast24 API",
     description="Backend-API for Forecast24 – prediksjon av strømpris neste 24 timer.",
     version="0.1.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # senere kan vi låse dette til forecast24.no
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
@@ -30,3 +39,4 @@ def get_forecast():
         "status": "placeholder",
         "message": "Forecast-endepunktet er satt opp, men ingen modell er koblet til enda.",
     }
+
