88b76952f8b7d35f31c22178c5314d769547e55f
Sondre Espe <Sondreespe02@gmail.com>
2025-12-12 16:37:13 +0100

    added middleware

--- app/main.py
+++ app/main.py
@@ -16,7 +16,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://forecast24-frontend.onrender.com",
+        # senere: "https://DITT-DOMENE.NO"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
